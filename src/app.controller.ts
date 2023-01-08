c3be8586f10c090f6dc2b210bccf4b41b0707a7e
YoungKi Lyu <youngkiu@gmail.com>
2023-01-08 15:37:37 +0900

    Add index.hbs for SSR

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('oauth')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get()
+  @Render('index')
+  root() {
+    return {
+      data: {
+        title: 'Chattings',
+        copyright: 'ryu jun yeol',
+      },
+    };
   }
 }
