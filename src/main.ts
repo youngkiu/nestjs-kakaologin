a28594e12545289bb204d342b493d3a1a91a8f35
YoungKi Lyu <youngkiu@gmail.com>
2023-02-06 00:23:23 +0900

    Config the port number as an environment variable

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,7 @@ async function bootstrap() {
 
   app.use(cookieParser());
 
-  await app.listen(3000);
+  const port = configService.get<number>('PORT') || 3000;
+  await app.listen(port);
 }
 bootstrap();
